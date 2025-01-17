74cd516fafe3014428bd11503aae8880d13ecd27
Sampathcode12 <lsampathlk4@gmail.com>
2025-01-17 22:24:18 +0530

    if want to buy somthing user should loging first

--- page/buy_now.php
+++ page/buy_now.php
@@ -1,6 +1,13 @@
 <?php
 include("Database.php");
 
+session_start();
+if (!isset($_SESSION["user_id"])) {
+    header("Location: login.php");
+    exit();
+}
+
+
 if (isset($_GET['product_id'])) {
     $product_id = $_GET['product_id'];
 
