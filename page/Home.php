b7443d3ae7ff22b5ef39fd42fbc58a98fa4a5dcd
Sampathcode12 <lsampathlk4@gmail.com>
2025-01-18 20:12:20 +0530

    after link product image with url

--- page/Home.php
+++ page/Home.php
@@ -34,16 +34,18 @@ $result = $conn->query($sql);
             <?php if ($result->num_rows > 0) : ?>
                 <?php while ($row = $result->fetch_assoc()) : ?>
                     <div class="product-card">
-                        <img src="<?php echo $row['image_path']; ?>" alt="<?php echo $row['name']; ?>" class="product-image">
-                        <a href="buy_now.php?product_id=<?php echo $row['id']; ?>" class="buy-button">Buy Now</a>
-                        <div class="product-details" >
-                            <h3 class="product-name"><?php echo $row['name']; ?></h3>
-                            <p class="product-category">Category: <?php echo ucfirst($row['category']); ?></p>
-                            <p class="product-price">$<?php echo number_format($row['price'], 2); ?></p>
-                            <p class="product-description"><?php echo substr($row['description'], 0, 50) . '...'; ?></p>
-                            <a href="buy_now.php?product_id=<?php echo $row['id']; ?>" class="buy-button">Buy Now</a>
-                        </div>
-                    </div>
+    <a href="product_details.php?product_id=<?php echo $row['id']; ?>">
+        <img src="<?php echo $row['image_path']; ?>" alt="<?php echo $row['name']; ?>" class="product-image">
+    </a>
+    <div class="product-details">
+        <h3 class="product-name"><?php echo $row['name']; ?></h3>
+        <p class="product-category">Category: <?php echo ucfirst($row['category']); ?></p>
+        <p class="product-price">$<?php echo number_format($row['price'], 2); ?></p>
+        <p class="product-description"><?php echo substr($row['description'], 0, 50) . '...'; ?></p>
+        <a href="buy_now.php?product_id=<?php echo $row['id']; ?>" class="buy-button">Buy Now</a>
+    </div>
+</div>
+
                 <?php endwhile; ?>
             <?php else : ?>
                 <p>No products available.</p>
