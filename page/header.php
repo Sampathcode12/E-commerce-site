6f85b513b9303e64ade2874ac95f9b6262379935
Sampathcode12 <lsampathlk4@gmail.com>
2025-01-18 16:35:02 +0530

    after edit navigation bar

--- page/header.php
+++ page/header.php
@@ -1,4 +1,20 @@
  <!-- Header Floater -->
+
+ <!DOCTYPE html>
+ <html lang="en">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Display</title>
+    <!-- Font Awesome for icons -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="styles.css">
+
+  </head>
+ 
+ 
  <header class="header">
     <div class="logo">
         <h1>NIKAN</h1>
@@ -23,3 +39,5 @@
         </a>
     </div>
 </header>
+
+</html>
