d9ae6502344511378b74e89187d36e1196314c1a
Sampathcode12 <lsampathlk4@gmail.com>
2025-01-22 13:19:37 +0530

    after create chack oders for seller

--- page/selle_pannel_hedear.php
+++ page/selle_pannel_hedear.php
@@ -6,7 +6,8 @@
             <li><a href="dashboard.php">Dashboard</a></li>
             <li><a href="seller_profile.php">Profile</a></li>
             <li><a href="Products.php">Products</a></li>
-            <li><a href="Contact.php">Orders</a></li>
+            <li><a href="Contact.php">contct</a></li>
+            <li><a href="seller_to_order.php">Orders</a></li>
             <li><a href="seller.php">Home</a></li>
            
 
